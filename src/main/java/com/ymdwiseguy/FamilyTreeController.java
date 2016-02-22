935c05ae3e3e664b43065597c4a48dda2ce53a33
Matthias Diekmann <matthias.diekmann@rewe-digital.com>
2016-02-22 18:21:01 +0100

    introduced basic usage of handlebars templates

--- src/main/java/com/ymdwiseguy/FamilyTreeController.java
+++ src/main/java/com/ymdwiseguy/FamilyTreeController.java
@@ -1,20 +1,46 @@
 package com.ymdwiseguy;
 
+import com.github.jknack.handlebars.Handlebars;
+import com.github.jknack.handlebars.Template;
+import com.github.jknack.handlebars.io.ClassPathTemplateLoader;
+import com.github.jknack.handlebars.io.TemplateLoader;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
 @RestController
 public class FamilyTreeController {
+
+    private static final Logger LOGGER = getLogger(FamilyTreeController.class);
 
     @Autowired
     FamilyTreeService familyTreeService;
 
+    @Value("${handlebars.prefix}")
+    String templatePrefix;
+
     @RequestMapping(value = "/greeting")
-    public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
-        model.addAttribute("name", name);
-        return familyTreeService.render();
+    public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+        TemplateLoader loader = new ClassPathTemplateLoader();
+        loader.setPrefix(templatePrefix);
+        Handlebars handlebars = new Handlebars(loader);
+
+        try {
+            LOGGER.info("rendering template");
+
+            Template template = handlebars.compile("index");
+            return template.apply(name);
+        } catch (IOException e) {
+            LOGGER.error("could not load template file", e);
+        }
+
+        return null;
     }
 }
